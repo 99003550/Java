5ff36751c4307e10837cab23ce0edce3545c3b0b
99003550 <78560502+99003550@users.noreply.github.com>
2021-03-26 14:52:37 +1100

    Add files via upload

---
+++ Exception/src/com/ltts/RunRate.java
@@ -0,0 +1,26 @@
+package com.ltts;
+import java.util.Scanner;
+
+public class RunRate 
+{
+	public static void main(String[] args) {
+		float tr,to;
+		String str,sto;
+		Scanner sc=new Scanner(System.in);
+		try
+		{
+			System.out.println("Enter the total runs scored:");
+			str=sc.next();
+			 tr = Integer.parseInt(str);  
+			System.out.println("Enter the total overs faced:");
+			sto=sc.next();
+			to = Integer.parseInt(sto);  
+			float rr=tr/to;
+			System.out.printf("Current Run Rate:%.2f",rr);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getClass());
+		}
+	}
+}
